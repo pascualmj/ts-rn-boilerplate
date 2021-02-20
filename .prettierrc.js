a0bf1d2229f82b871560ff2c0722c84bbbe9b75d
Manuel Pascual <manupascual90@gmail.com>
2021-02-20 13:20:38 -0300

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  arrowParens: 'avoid'
+}
