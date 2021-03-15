d145646a8b85e6bedb68c4810f64d76c292d3ce4
Manuel Pascual <manupascual90@gmail.com>
2021-03-14 21:54:48 -0300

    feat: create and setup error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ import store from './store'
 import ThemeProvider from './services/themes/context/ThemeContext'
 import {navigationRef} from './services/navigation'
 import RootNavigator from './navigation/RootNavigator'
+import BaseErrorBoundary from './components/baseErrorBoundary/BaseErrorBoundary'
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <NavigationContainer ref={navigationRef}>
-          <RootNavigator />
-        </NavigationContainer>
+        <BaseErrorBoundary>
+          <NavigationContainer ref={navigationRef}>
+            <RootNavigator />
+          </NavigationContainer>
+        </BaseErrorBoundary>
       </ThemeProvider>
     </Provider>
   )
---
+++ src/components/baseErrorBoundary/BaseErrorBoundary.tsx
@@ -0,0 +1,55 @@
+import React, {Component, ErrorInfo, ReactNode} from 'react'
+import {View, Text, Button, StyleSheet} from 'react-native'
+import RNRestart from 'react-native-restart'
+
+interface IErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface IErrorBoundaryState {
+  error: boolean
+}
+
+class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
+  public state: IErrorBoundaryState = {
+    error: false
+  }
+
+  public static getDerivedStateFromError(_: Error): IErrorBoundaryState {
+    // Update state so the next render will show the fallback UI
+    return {error: true}
+  }
+
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    // Log info about the error to an external service
+  }
+
+  public restartApp(): void {
+    // Wipe user data from async storage and restart the app
+    RNRestart.Restart()
+  }
+
+  public render(): ReactNode {
+    if (this.state.error) {
+      return (
+        <View style={styles.wrapper}>
+          <Text>Oops! Algo salió mal...</Text>
+          <Button onPress={this.restartApp} title="Volver" />
+        </View>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default ErrorBoundary
