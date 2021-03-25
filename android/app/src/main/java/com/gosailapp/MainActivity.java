a6aaf84ac7d0fe325552dce2519f5058224cbf37
Manuel Pascual <manupascual90@gmail.com>
2021-03-25 20:56:18 -0300

    refactor: change app name

--- android/app/src/main/java/com/gosailapp/MainActivity.java
+++ android/app/src/main/java/com/gosailapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "GosailApp";
+    return "RNTSBoilerplate";
   }
 }
--- ios/RNTSBoilerplateTests/RNTSBoilerplateTests.m
+++ ios/RNTSBoilerplateTests/RNTSBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GosailAppTests : XCTestCase
+@interface RNTSBoilerplateTests : XCTestCase
 
 @end
 
-@implementation GosailAppTests
+@implementation RNTSBoilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
