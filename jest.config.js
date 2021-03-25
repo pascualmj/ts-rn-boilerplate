ff2bac3a18a0a2a6da5cc451cab797e80f8513e8
Manuel Pascual <manupascual90@gmail.com>
2021-03-25 20:15:30 -0300

    feat: jest coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/src/**/__tests__/**/*.[jt]s?(x)'],
-  clearMocks: true
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.{ts,tsx}',
+    '<rootDir>/src/screens/**/*.{ts,tsx}',
+    '<rootDir>/src/hooks/**/*.{ts,tsx}',
+    '<rootDir>/src/services/**/*.{ts,tsx}',
+    '<rootDir>/src/store/**/*.{ts,tsx}',
+    '<rootDir>/src/utils/**/*.{ts,tsx}'
+  ]
 }
