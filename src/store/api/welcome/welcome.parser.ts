9d7e7a19521ade15d552b80e56900154487a6439
Manuel Pascual <manupascual90@gmail.com>
2021-03-31 20:42:16 -0300

    refactor: api parser service

--- src/store/api/welcome/welcome.parser.ts
+++ src/store/api/welcome/welcome.parser.ts
@@ -3,9 +3,14 @@ import {IFetchWelcomeResponse, IFetchWelcomeParsed} from './welcome.schemes'
 
 const welcomeParser = ApiParser.create('welcomeParser')
 
-welcomeParser.addParser<IFetchWelcomeResponse, IFetchWelcomeParsed>('fetchWelcome', input => ({
-  primary: input.data.result.title,
-  count: input.data.result.title.length
-}))
+export const PARSER_FETCH_WELCOME = 'fetchWelcome'
+
+welcomeParser.addParser<IFetchWelcomeResponse, IFetchWelcomeParsed>(
+  PARSER_FETCH_WELCOME,
+  input => ({
+    primary: input.data.result.title,
+    count: input.data.result.title.length
+  })
+)
 
 export default welcomeParser
--- src/store/api/welcome/welcome.ts
+++ src/store/api/welcome/welcome.ts
@@ -1,9 +1,9 @@
 import ApiClient from '../../../services/apiClient'
 import {IFetchWelcomeParsed} from './welcome.schemes'
-import welcomeParser from './welcome.parser'
+import welcomeParser, {PARSER_FETCH_WELCOME} from './welcome.parser'
 
 export const fetchWelcome = async (): Promise<IFetchWelcomeParsed> => {
-  return welcomeParser.parse('fetchWelcome', await ApiClient.get(''))
+  return welcomeParser.parse(PARSER_FETCH_WELCOME, await ApiClient.get(''))
 }
 
 export * from './welcome.schemes'
